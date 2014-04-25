1643c5c1493bdd7a9a69d4d9b440bcf6f6ba5dc0
Kenner Kliemann <kenner.hp@gmail.com>
2014-04-24 21:48:01 -0300

    Exe 3 solved !

--- src/br/github/gnomex/some_examples/lists/Exe3.java
+++ src/br/github/gnomex/some_examples/lists/Exe3.java
@@ -1,5 +1,9 @@
 package br.github.gnomex.some_examples.lists;
 
+import java.util.Random;
+
+import br.github.gnomex.lists.SimpleLinkedList;
+
 	/**
 	 * Modificar a classe ListaDupEnc incluindo um método de ordenação
 	 * que ao ser chamado coloca os elementos da lista em ordem crescente
@@ -8,4 +12,43 @@ package br.github.gnomex.some_examples.lists;
 
 public class Exe3 {
 
+	private SimpleLinkedList<String> list = new SimpleLinkedList<String>();
+	
+	public void populeteWithDummieStubs()	{
+		
+		Random rand = new Random(1000);
+		
+		for (int i = 0; i < 100; i++) {
+			list.add("Marmot " + i + rand.nextInt());
+		}
+	}
+	
+	public void showMarmots()	{
+		for (String no : list) {
+			System.out.println(" -" + no);
+		}
+	}
+	
+	public void doSort()	{
+		
+	}
+	
+	public static void main(String[] args) {
+		Exe3 exe3 = new Exe3();
+		
+		try {
+			
+			exe3.populeteWithDummieStubs();
+			exe3.showMarmots();
+			exe3.doSort();
+			
+			System.out.println(" #=> Ordened");
+			
+			exe3.showMarmots();
+			
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+	}
+	
 }
